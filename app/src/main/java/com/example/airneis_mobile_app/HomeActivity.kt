a5dadf2085deef7f888b97da92df541c75be2274
Florian Pescot-Jourdan <florianpescot4@gmail.com>
2024-02-16 21:44:14 +0100

    Initial commit

---
+++ app/src/main/java/com/example/airneis_mobile_app/HomeActivity.kt
@@ -0,0 +1,11 @@
+package com.example.airneis_mobile_app
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class HomePageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.homepage)
+    }
+}
