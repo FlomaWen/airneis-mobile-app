1903205e54d1a9efb16739b15996a94e82283a28
Florian Pescot--Jourdan <florianpescot4@gmail.com>
2024-03-04 12:23:44 +0100

    Page panier

---
+++ app/src/main/java/com/example/airneis_mobile_app/CartActivity.kt
@@ -0,0 +1,12 @@
+package com.example.airneis_mobile_app
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class CartActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.cart_activity)
+    }
+}
--- app/src/main/java/com/example/airneis_mobile_app/HomeActivity.kt
+++ app/src/main/java/com/example/airneis_mobile_app/HomeActivity.kt
@@ -1,11 +1,20 @@
 package com.example.airneis_mobile_app
 
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 
 class HomePageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.homepage)
+
+        // Cart section
+        val cartbutton = findViewById<ImageButton>(R.id.imageButton3)
+        cartbutton.setOnClickListener {
+            val intent = Intent(this,CartActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
